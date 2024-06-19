95c0a34a19c1e925f87c3c328a1ef932ed2a1a52
PritiJohnes <pritijohnes30@gmail.com>
2024-06-19 20:14:09 +0530

    changed the bottom bar

--- src/bottombar.js
+++ src/bottombar.js
@@ -13,7 +13,7 @@ const Bottombar = ()=>{
             <Text style={{textDecoration:"none", color:"#fff"}}> <a href="mailto:mercyhospital@gmail.com" style={{textDecoration:"none", color:"#fff"}}><i class="fa fa-envelope" aria-hidden="true"></i> &nbsp;</a> </Text>
             </Group>:
             <Group justify="center" h={50} bg="#000" c="#fff" w="100%">
-            <Text>© 2024 Mercy Hospital | all rights reserved | Designed by Sandeep Kisku || &nbsp; &nbsp;</Text>
+            <Text>© 2024 Mercy Hospital | all rights reserved &nbsp; &nbsp;</Text>
             <Text > <a style={{textDecoration:"none", color:"#2f3082", fontSize:"18px", width:"100px"}} href="https://www.youtube.com/@MERCYHOSPITALPOREYAHAT"><i class="fa fa-youtube" aria-hidden="true" style={{ color:"red"}}></i>  &nbsp; </a> </Text>
             <Text style={{textDecoration:"none", color:"#4285F4"}}> <a style={{textDecoration:"none", color:"#2f3082"}} href="https://m.facebook.com/people/Mercy-Hospital-Poreyahat/100091387437222/" ><i class="fa fa-facebook" aria-hidden="true" style={{color:"#1877F2"}}></i>  &nbsp; &nbsp; </a> </Text>
             <Text style={{textDecoration:"none", color:"#fff"}}> <a href="mailto:mercyhospital@gmail.com" style={{textDecoration:"none", color:"#fff"}}><i class="fa fa-envelope" aria-hidden="true"></i> &nbsp;</a> </Text>
